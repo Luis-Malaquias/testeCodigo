9541b3853d79253f3b3169d9bed54b48ddd26598
Luis <lfrmalaquias10@gmail.com>
2023-07-05 14:54:34 -0300

    Primeira main

---
+++ testeCodigo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char string1 [100];
+    printf("1- Reverta a ordem das palavaras nas frases, mantendo a ordem das palavras:\n");
+    scanf("%s", string1);
+
+    char string2 [100];
+    printf("2- Remova todos os caracteres da string a baixo:\n");
+    scanf("%s", string2);
+
+    char string3 [100];
+    printf("3- Encontre a substring mais longa na string a baixo:\n");
+    scanf("%s", string3);
+
+    char string4 [100];
+    printf("4-Coloque maiuscula na primeira letra de cada frase da string:\n");
+    scanf("%s", string4);
+ 
+    char string5 [100];
+    printf("5- Verifique se a string é anagrama de um palindromo:\n");
+    scanf("%s", string5);
+}
